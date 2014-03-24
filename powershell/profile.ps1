997aeb1f36ad45a5a6183a064a45b189f6f61146
bpugh <bp@brandonpugh.com>
2014-03-24 13:43:25 -0500

    Add powershell func to set git user email config
    I usually use this at work since the default I have set globally isn't what
    I want to use for my personal repos.

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -11,6 +11,8 @@ Function ci { git ci $args }
 Function lol { git lol $args }
 Function rsh { git reset --hard $args }
 Function gconf { gvim C:\Users\bpugh\.gitconfig }
+Function gitemail { git config user.email "bp@brandonpugh.com" }
+
 
 #For deleting dir/files with really long paths
 function scrub {
